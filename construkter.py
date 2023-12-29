b7f66c84a791c8d4cc0567e2092cd4a8b756c4a0
Rillikminecraft <151042075+Rillikminecraft@users.noreply.github.com>
2023-12-29 21:21:16 +0300

    construkter.py

--- construkter.py
+++ construkter.py
@@ -1,9 +1,9 @@
 import os 
-
+# это только в разработке хз чо там будет
 py_file = open("1.py", "w")
 
 py_file.write("import telebot\nfrom telebot import types\n\nbot = telebot.TeleBot('')\n\n@bot.message_handler(commands=['start'])\ndef start(message):\n    bot.send_message(message.chat.id, 'Бот запущен!')")
 
-open("1.py", "w").write("Это текстовый файл")
+open("1.py", "w").write("ПОН")
 
 print(os.stat("1.py"))
